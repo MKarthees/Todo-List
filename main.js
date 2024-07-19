a7a751ff1c43b59f212180f0bf05c127c8db62fa
Karthees <kartheeswaran68@gmail.com>
2024-07-19 22:41:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+var addbtn = document.querySelector(".add-button");
+var todo_insert = document.querySelector(".todo-insert");
+var overlay = document.querySelector(".overlay");
+var list_align = document.querySelector(".list-align");
+var cancle = document.querySelector(".cancle");
+var add_task = document.querySelector(".add-task");
+var field1 = document.querySelector(".field1");
+var field2 = document.querySelector(".field2");
+var field3 = document.querySelector(".field3");
+var list = document.querySelector(".list");
+var list1 = document.querySelector(".list2");
+var list2 = document.querySelector(".list3");
+var list3 = document.querySelector(".list4");
+var list4 = document.querySelector(".list5");
+function show() {
+  overlay.style.display = "block";
+  todo_insert.style.display = "block";
+}
+
+function unshow() {
+  overlay.style.display = "none";
+  todo_insert.style.display = "none";
+}
+function addtask() {
+  if (field1.value === "") {
+    alert("This Is Empty");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = field1.value;
+    list.appendChild(li);
+    let i = document.createElement("i");
+    i.innerHTML = "<i class='fa fa-pen-to-square'></i>";
+    list3.appendChild(i);
+    i.innerHTML = "<i class='fa fa-trash'></i>";
+    list4.appendChild(i);
+  }
+  field1.value = "";
+
+  if (field2.value === "") {
+    alert("This Is Empty");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = field2.value;
+    list1.appendChild(li);
+  }
+  field2.value = "";
+
+  if (field3.value === "") {
+    alert("This Is Empty");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = field3.value;
+    list2.appendChild(li);
+    overlay.style.display = "none";
+    todo_insert.style.display = "none";
+  }
+  field3.value = "";
+}
